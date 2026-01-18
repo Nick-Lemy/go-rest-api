f71ccc6a612f73366fd2d8793f7370af8ecb2605
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-18 18:50:10 +0200

    feat: implement user service and handler for user management

---
+++ cmd/rest_api/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	serve "github.com/yourGithubUsername/gin-rest-tutorial/api/server"
+	routes "github.com/yourGithubUsername/gin-rest-tutorial/api/server/router"
+	"github.com/yourGithubUsername/gin-rest-tutorial/configs"
+	"github.com/yourGithubUsername/gin-rest-tutorial/internal/app/rest_api/database"
+	"github.com/yourGithubUsername/gin-rest-tutorial/internal/app/rest_api/handlers"
+	"github.com/yourGithubUsername/gin-rest-tutorial/internal/app/rest_api/repositories"
+	"github.com/yourGithubUsername/gin-rest-tutorial/internal/app/rest_api/services"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	config := configs.NewConfig()
+
+	client, err := database.NewSQLClient(database.Config{
+		DBDriver:          config.Database.DatabaseDriver,
+		DBSource:          config.Database.DatabseSource,
+		MaxOpenConns:      25,
+		MaxIdleConns:      25,
+		ConnMaxIdleTime:   15 * time.Minute,
+		ConnectionTimeout: 5 * time.Second,
+	})
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize database client")
+		return
+	}
+
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Error().Msgf("Failed to close database client: %v", err)
+		}
+	}()
+
+	// Initialize repositories
+	userRepo := repositories.NewUserRepository(client.DB)
+
+	//Initialize services
+	userService := services.NewUserService(userRepo)
+
+	// Pass services to handlers
+	userHandler := handlers.NewUserHandler(userService)
+
+	cors := config.CorsNew()
+
+	router := gin.Default()
+	router.Use(cors)
+
+	// Register routes
+	routes.RegisterPublicEndpoints(router, userHandler)
+
+	server := serve.NewServer(log.Logger, router, config)
+	server.Serve()
+}
