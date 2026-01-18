8e13d083c7f846a1dd24baa36120e06867551c13
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-18 18:22:05 +0200

    feat: add configuration and constants for server and database setup

---
+++ configs/config.go
@@ -0,0 +1,65 @@
+package configs
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/yourGithubUsername/gin-rest-tutorial/internal/app/rest_api/constants"
+)
+
+type Config struct {
+	Server   serverConfig
+	Database databaseConfig
+}
+
+type serverConfig struct {
+	Address string
+}
+
+type databaseConfig struct {
+	DatabaseDriver string
+	DatabseSource  string
+}
+
+func NewConfig() *Config {
+	err := godotenv.Load("configs/dev.env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	c := &Config{
+		Server: serverConfig{
+			Address: GetEnvOrPanic(constants.EnvKeys.ServerAdress),
+		},
+		Database: databaseConfig{
+			DatabaseDriver: GetEnvOrPanic(constants.EnvKeys.DBDriver),
+			DatabseSource:  GetEnvOrPanic(constants.EnvKeys.DBSource),
+		},
+	}
+	return c
+}
+
+func GetEnvOrPanic(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		panic(fmt.Sprintf("environment variable %s not set", key))
+	}
+	return value
+}
+
+func (conf *Config) CorsNew() gin.HandlerFunc {
+	allowedOrigin := GetEnvOrPanic(constants.EnvKeys.CorsAllowedOrigin)
+	return cors.New(cors.Config{
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+		AllowHeaders:     []string{constants.Headers.Origin},
+		ExposeHeaders:    []string{constants.Headers.ContentLength},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == allowedOrigin
+		},
+		MaxAge: constants.MaxAge,
+	})
+}
