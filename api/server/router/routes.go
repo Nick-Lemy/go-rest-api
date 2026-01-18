371bfe9c2ee5f1e2db3a1bf337b323028e3a0769
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-18 18:38:03 +0200

    feat: add user management endpoints to the router

---
+++ api/server/router/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterPublicEndpoints(router *gin.Engine, userHandlers *handlers.User) {
+	router.GET("/users", userHandlers.GetAllUsers)
+	router.GET("/users/:id", userHandlers.GetUser)
+	router.POST("/users", userHandlers.CreateUser)
+	router.PUT("/users/:id", userHandlers.UpdateUser)
+	router.DELETE("/users/:id", userHandlers.DeleteUser)
+}
