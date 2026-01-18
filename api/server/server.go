4162722ab6c9c22d946bd95a47cdde51ed829d5a
Nick-Lemy <n.kayiranga@alustudent.com>
2026-01-18 18:34:36 +0200

    feat: implement server structure with graceful shutdown

---
+++ api/server/server.go
@@ -0,0 +1,54 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/yourGithubUsername/gin-rest-tutorial/configs"
+)
+
+type Server struct {
+	l      zerolog.Logger
+	router *gin.Engine
+	config *configs.Config
+}
+
+func NewServer(l zerolog.Logger, router *gin.Engine, config *configs.Config) *Server {
+	return &Server{l: l, router: router, config: config}
+}
+
+func (s *Server) Serve() {
+	srv := &http.Server{
+		Addr:    s.config.Server.Address,
+		Handler: s.router.Handler(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			s.l.Fatal().Err(err).Msg("Listen")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	s.l.Info().Msg("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		s.l.Fatal().Err(err).Msg("Server Shutdown")
+	}
+
+	<-ctx.Done()
+	s.l.Info().Msg("Server shutdown timeout of 30 seconds")
+	s.l.Info().Msg("Server exiting")
+}
